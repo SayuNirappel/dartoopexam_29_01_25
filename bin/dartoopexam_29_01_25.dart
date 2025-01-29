5c338e510f631071ec288a4320516d3cbca93f29
SayuNirappel <sayunirappel@gmail.com>
2025-01-29 10:51:17 +0530

    first commit

---
+++ bin/dartoopexam_29_01_25.dart
@@ -0,0 +1,6 @@
+import 'package:dartoopexam_29_01_25/dartoopexam_29_01_25.dart'
+    as dartoopexam_29_01_25;
+
+void main() {
+  print('Hello world: !');
+}
---
+++ test/dartoopexam_29_01_25_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartoopexam_29_01_25/dartoopexam_29_01_25.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
